66f9ca51c74c0a002a3f6425b31d97e6e1a87b87
MrazTevin <millatevin@gmail.com>
2025-04-25 22:22:27 +0300

    (chore): update favicon

--- nextjs-frontend/src/app/layout.tsx
+++ nextjs-frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react";
 import type { Metadata } from "next";
 import {Inter, Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -27,6 +28,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body
         className={inter.className} 
       >
